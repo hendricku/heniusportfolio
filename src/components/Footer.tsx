835b21fa2f8967d5b441465f5319c4f06a662d5c
hendricku <130520855+hendricku@users.noreply.github.com>
2025-06-04 21:20:53 +0800

    pushhhhingggggggg new footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="w-full bg-white/95 backdrop-blur-lg border-t border-gray-100 py-6 mt-20">
       <div className="max-w-7xl mx-auto px-4 flex flex-col items-center">
         <p className="text-gray-600 text-sm">
-          © {new Date().getFullYear()} All Rights Reserved | Hendrick Castro
+          © {new Date().getFullYear()} All Rights Reserved | Hendrick Paul Llamas Castro
         </p>
       </div>
     </footer>
